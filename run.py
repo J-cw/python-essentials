8d4331e375e339724ebd18d8478e616beda2b1dd
J-cw <jacob.cw@outlook.com>
2022-10-16 01:06:50 +0000

    added base hangman scaffold function that shows different stages of the game

--- run.py
+++ run.py
@@ -1,3 +1,70 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def print_scaffold(gueses, word):
+    if (guesses == 0):
+        print("_____________")
+        print("|      |")
+        print("|")
+        print("|")
+        print("|")
+        print("|")
+        print("|_____________")
+    elif (guesses == 1):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|")
+        print("|")
+        print("|")
+        print("|_____________")
+    if (guesses == 2):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|      |")
+        print("|      |")
+        print("|")
+        print("|____________")
+    if (guesses == 3):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|     /|")
+        print("|      |")
+        print("|")
+        print("|____________")
+    if (guesses == 4):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|     /|\ ")
+        print("|      |")
+        print("|")
+        print("|____________")
+    if (guesses == 5):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|     /|\ ")
+        print("|      |")
+        print("|     / ")
+        print("|____________")
+    if (guesses == 6):
+        print("_____________")
+        print("|      |")
+        print("|      o")
+        print("|     /|\ ")
+        print("|      |")
+        print("|     / \ ")
+        print("|____________")
+        print("\n")
+        print("The word was " + word + ".")
+        print("\n")
+        print("YOU LOSE!")
+        print("Do you want to play again?(Yes/No)")
+        again = input(":").lower()
+        if again == "yes":
+            hangman()
+        elif again == "no":
+            quit()
